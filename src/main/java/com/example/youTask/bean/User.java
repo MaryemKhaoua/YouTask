99e590dbf65ccc8c5e7ae50b27972580c62a608f
Maryem khaoua <maryem.khaoua@gmail.com>
2024-10-02 11:35:39 +0100

    creer entite user

--- src/main/java/com/example/youTask/bean/User.java
+++ src/main/java/com/example/youTask/bean/User.java
@@ -11,16 +11,90 @@ public class User {
 
     @Column(name = "username", nullable = false, unique = true)
     private String username;
+
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
-    @Column(name = "lastName", nullable = false)
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
-    
 
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private User manager;
 
+    public User() {
+
+    }
+
+    public User(Long id, String username, String password, String firstName, String lastName, String email, User manager) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.manager = manager;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User getManager() {
+        return manager;
+    }
+
+    public void setManager(User manager) {
+        this.manager = manager;
+    }
 }

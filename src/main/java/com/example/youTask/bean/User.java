d080e3ebe84d0b2f1fd02ab4dc1fe22ce099e6a1
Maryem khaoua <maryem.khaoua@gmail.com>
2024-10-02 11:09:05 +0100

    creer entite user

--- src/main/java/com/example/youTask/bean/User.java
+++ src/main/java/com/example/youTask/bean/User.java
@@ -1,6 +1,26 @@
 package com.example.youTask.bean;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    
 
 
 }
